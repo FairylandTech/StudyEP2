dace0402a8d5d236902518a616d11ff6772a2459
AliceEnginee(Private) <alice.engineer.pro@outlook.com>
2022-06-03 21:32:47 +0800

    增加clash的订阅脚本

---
+++ ShellScript/Clash/clash_sub.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# 局域网
+
+# 订阅可可云
+sub_kk_cloud() {
+  echo -e "Start downloads Sub link!"
+  wget -O sub.yaml.src "$1"
+  cp sub.yaml.src sub.yaml
+  sed -i '1i mixed-port: 1080' sub.yaml
+  sed -i 's/log-level: info/log-level: debug/g' sub.yaml
+  cp sub.yaml config.yaml
+}
+
+# 重启Clash
+restart_clash() {
+  if [ "$(netstat -ntlp | grep -c 1080)" == 1 ]; then
+    kill "$(pgrep clash | awk '{print $1}')"
+    sleep 10
+    clash > ~/file/logs/clash.log &
+  else
+    clash > ~/file/logs/clash.log &
+  fi
+}
+
+# __main__
+cd ~/.config/clash || mkdir -p ~/.config/clash && cd ~/.config/clash || exit
+if [ "$(find ./ -name "Country.mmdb" | wc -l)" == 0 ]; then
+  echo -e "State Downloads \"Country.mmdb\" file"
+  wget "https://file.share.alicehome.ltd/data/file/clash/Country.mmdb"
+  sub_kk_cloud "$1"
+  echo -e "Sub successfully. Now, Start Clash... (ps. At least 10s)"
+  restart_clash
+elif [ "$(find ./ -name "Country.mmdb" | wc -l)" == 1 ]; then
+  sub_kk_cloud "$1"
+  echo -e "Sub successfully. Now, Start Clash... (ps. At least 10s)"
+  restart_clash
+else
+  echo -e "Please check the path to the \"Country.mmdb\" file"
+fi
---
+++ main.py
@@ -0,0 +1,8 @@
+from rich.console import Console
+
+console = Console()
+
+name = 'https://127.0.0.1:1080'
+http = 'http://127.0.0.1:1080'
+# console.print('[red]程序即将退出！')
+console.print(r'[red]{name}')
