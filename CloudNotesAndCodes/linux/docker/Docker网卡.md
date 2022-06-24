6b260e76e5e2c975ca4375cd7c758eb7a3bc61b1
AliceEngineer <private.profession@foxmail.com>
2022-06-24 11:06:42 +0800

    :sparkles:WorkSpace

--- CloudNotesAndCodes/linux/docker/Docker网卡.md
+++
@@ -1,8 +0,0 @@
-# Docker网卡
-
-1. 建立网卡
-
-```shell
-docker network create --driver bridge --subnet 111.11.0.0/24 --gateway 111.11.0.1 docker_net
-```
-
--- PythonScript/pip/update_pip.py
+++
@@ -1,6 +0,0 @@
-import pkg_resources
-from subprocess import call
-
-for packages in [dist.project_name for dist in pkg_resources.working_set]:
-    print(packages)
-    call("pip install --upgrade " + ''.join(packages) + ' --user', shell=True)
