bcaaf48ebc65b8c7e91815ec31270725546c31cb
AliceEngineer <private.profession@foxmail.com>
2022-06-24 11:08:04 +0800

    :sparkles:Release

--- WorkSpace/Script/SystemProxy/running.proxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/.config/clash/
-cp Country.mmdb ~/.config/clash/
-yum install wget -y
-wget -O ~/.config/clash/config.yaml $1
-gzip -d clash-linux-amd64-v1.10.6.gz
-cp clash-linux-amd64-v1.10.6 /usr/local/bin/clash
-clash
