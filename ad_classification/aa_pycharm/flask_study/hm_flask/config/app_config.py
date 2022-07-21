878c29c1f915ce6595caa9d3f25d7b3315f16602
AliceEngineerJob(WorkSpace) <alice_job@yeah.net>
2022-07-21 19:27:27 +0800

    Flask

--- ad_classification/aa_pycharm/flask_study/hm_flask/config/app_config.py
+++ ad_classification/aa_pycharm/flask_study/hm_flask/config/app_config.py
@@ -1,3 +1,19 @@
-class DefaultConfig(object):
+class DefaultConfig:
+    
+    def __init__(self):
+        pass
+    
     MYSQL_URL = 'mysql address'
     REDIS_URL = 'redis address'
+    
+
+class ProductionConfig:
+    
+    def __init__(self):
+        pass
+    
+    
+    MYSQL_URL = 'produce mysql address'
+    REDIS_URL = 'produce redis address'
+
+
---
+++ ad_classification/aa_pycharm/flask_study/hm_flask/factory_moduls.py
@@ -0,0 +1,17 @@
+from run import Flask
+from config import app_config
+
+# factory modules 
+class FactoryModules:
+    
+    def __init__(self):
+        pass
+    
+    
+    @staticmethod
+    def create_study(config_name):
+        study = Flask(__name__)
+        study.config.from_object(config_name)
+        return study
+    
+    
