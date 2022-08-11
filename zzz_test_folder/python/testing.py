46824b09c7861c3f867b0f1927baadee8ac30151
AliceEngineer <private.profession@foxmail.com>
2022-08-11 15:41:03 +0800

    猜数字

--- zzz_test_folder/python/testing.py
+++ zzz_test_folder/python/testing.py
@@ -1,8 +1,20 @@
 # encoding=utf-8
 
-while True:
+import random
 
-    num = float(input('num='))
-    result = num / 1024 / 8 * 1024
-    print(result)
-    print()
+random_num = random.randint(1, 50)
+
+for i in range(1, 6):
+    input_num = int(input(f'第{i}次: 输入一个数'))
+    if 1 <= input_num <= 50:
+        if input_num == random_num:
+            print('猜对了')
+            break
+        elif input_num < random_num:
+            print('猜小了')
+        else:
+            print('猜大了')
+    else:
+        print('输入错误, 请重新输入')
+    if i == 5:
+        print('次数已用完')
